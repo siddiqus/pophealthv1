b698cb5966da39720977c822a539f1e3e0970269
Sabbir Siddiqui <ssiddiqui@gosmcpartners.com>
2013-11-01 16:13:25 -0400

    updated run script with redis and mongod

--- runPopHealth.sh
+++ runPopHealth.sh
@@ -1,3 +1,4 @@
-mongod &
-~/Programs/redis-2.4.16/src/redis-server &
+sudo mongod --dbpath /var/lib/mongodb &
+~/bstrezze/redis/redis-2.6.16/src/redis-server &
 QUEUE=* bundle exec rake resque:work
+rails s
